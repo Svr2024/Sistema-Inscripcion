c9de2ac5a3c67698644746602b9b83bccf4d254e
SaraVega <saravega.if@gmail.com>
2025-05-26 19:34:06 -0400

    modifyclass

--- Estudiante.py
+++ Estudiante.py
@@ -1,9 +1,11 @@
 class Estudiante:
-    def __init__(self, cedula, nombre, carrera, prioridad, estado="pendiente"):
+    def __init__(self, cedula, nombre, carrera, prioridad,materias, estado="pendiente"):
         self.cedula = cedula
         self.nombre = nombre
         self.carrera = carrera
         self.prioridad = prioridad
+        self.materias = materias
+        
         self.estado = estado
 
     def get_info(self):
