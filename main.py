8838ebbe85531d71788ff9325cb5c85452907b89
LuisDCS <1001.27539960.ucla@gmail.com>
2025-05-27 08:08:33 -0400

    ventana de pilas agregadas a main

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import tkinter as tk
 from cola_tickets import VentanaTickets
 from lista_inscripcion import VentanaInscripcion
+from pila_materias import VentanaMaterias
 
 def abrir_ventana_inscripcion():
     root.withdraw()  
 
     ventana = VentanaInscripcion(root) 
+
+    def al_cerrar():
+        ventana.destroy()
+        root.deiconify()  
+
+    ventana.protocol("WM_DELETE_WINDOW", al_cerrar) 
+
+def abrir_ventana_materias():
+    root.withdraw()  
+
+    ventana = VentanaMaterias(root) 
 
     def al_cerrar():
         ventana.destroy()
@@ -20,5 +32,6 @@ tk.Label(root, text="Sistema de Inscripción UCLA", font=("Arial", 16)).pack(pad
 
 tk.Button(root, text="Cola de espera", width=30, command=VentanaTickets).pack(pady=5)
 tk.Button(root, text="Inscripción de Estudiantes ", width=30, command=abrir_ventana_inscripcion).pack(pady=5)
+tk.Button(root, text="Materias", width=30, command=abrir_ventana_materias).pack(pady=5)
 
 root.mainloop()
