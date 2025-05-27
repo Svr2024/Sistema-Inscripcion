7ac4553322be267f5d3a2f2bc1b519d9493e1cc8
LuisDCS <1001.27539960.ucla@gmail.com>
2025-05-27 08:08:33 -0400

    ventana de pilas agregadas a main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from cola_tickets import VentanaTickets
 from lista_inscripcion import VentanaInscripcion
+from pila_materias import VentanaMaterias
 
 
 root = tk.Tk()
@@ -10,6 +11,6 @@ tk.Label(root, text="Sistema de Inscripción UCLA", font=("Arial", 16)).pack(pad
 
 tk.Button(root, text="Cola de espera", width=30, command=VentanaTickets).pack(pady=5)
 tk.Button(root, text="Inscripción de Estudiantes ", width=30, command=VentanaInscripcion).pack(pady=5)
-
+tk.Button(root, text="Materias", width=30, command=VentanaMaterias).pack(pady=5)
 
 root.mainloop()
