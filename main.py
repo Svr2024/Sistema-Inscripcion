d82655b352d1c2bc896f53c713c214e5970cbec2
DESKTOP-ABU10GI\Ernesto <3rnestocs@gmail.com>
2025-05-29 20:52:12 -0400

    Centrado vertical y horizontal de ventana principal. Boton de materia eliminado de pantalla principal.

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ root.title("Sistema de Inscripción")
 # Establecer tamaño inicial de la ventana
 root.geometry("800x600")
 
+# Centrar ventana en la pantalla
+root.update_idletasks()
+width = 800
+height = 600
+x = (root.winfo_screenwidth() // 2) - (width // 2)
+y = (root.winfo_screenheight() // 2) - (height // 2)
+root.geometry(f"{width}x{height}+{x}+{y}")
+
 # Crear un marco para centrar los widgets
 frame = tk.Frame(root)
 frame.pack(expand=True)
@@ -28,6 +36,5 @@ tk.Label(frame, text="Sistema de Inscripción UCLA", font=("Arial", 16)).pack(pa
 
 tk.Button(frame, text="Cola de espera", width=30, command=VentanaTickets).pack(pady=5)
 tk.Button(frame, text="Inscripción de Estudiantes ", width=30, command=VentanaInscripcion).pack(pady=5)
-tk.Button(frame, text="Materias", width=30, command=VentanaMaterias).pack(pady=5)
 
 root.mainloop()
