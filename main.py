776914aa4884195f48c1ead24e6839e8faa7a2ab
DESKTOP-ABU10GI\Ernesto <3rnestocs@gmail.com>
2025-05-29 20:52:12 -0400

    Centrado vertical y horizontal de ventana principal. Boton de materia eliminado de pantalla principal.

--- main.py
+++ main.py
@@ -31,11 +31,14 @@ root.title("Sistema de Inscripción")
 # Establecer tamaño inicial de la ventana
 root.geometry("800x600")
 
-<<<<<<< HEAD
-tk.Button(root, text="Cola de espera", width=30, command=VentanaTickets).pack(pady=5)
-tk.Button(root, text="Inscripción de Estudiantes ", width=30, command=abrir_ventana_inscripcion).pack(pady=5)
-tk.Button(root, text="Materias", width=30, command=abrir_ventana_materias).pack(pady=5)
-=======
+# Centrar ventana en la pantalla
+root.update_idletasks()
+width = 800
+height = 600
+x = (root.winfo_screenwidth() // 2) - (width // 2)
+y = (root.winfo_screenheight() // 2) - (height // 2)
+root.geometry(f"{width}x{height}+{x}+{y}")
+
 # Crear un marco para centrar los widgets
 frame = tk.Frame(root)
 frame.pack(expand=True)
@@ -43,8 +46,7 @@ frame.pack(expand=True)
 tk.Label(frame, text="Sistema de Inscripción UCLA", font=("Arial", 16)).pack(pady=10)
 
 tk.Button(frame, text="Cola de espera", width=30, command=VentanaTickets).pack(pady=5)
-tk.Button(frame, text="Inscripción de Estudiantes ", width=30, command=VentanaInscripcion).pack(pady=5)
-tk.Button(frame, text="Materias", width=30, command=VentanaMaterias).pack(pady=5)
->>>>>>> 5081343 (Logica de inscribir materias añadida)
+tk.Button(frame, text="Inscripción de Estudiantes ", width=30, command=abrir_ventana_inscripcion).pack(pady=5)
+tk.Button(frame, text="Materias", width=30, command=abrir_ventana_materias).pack(pady=5)
 
 root.mainloop()
